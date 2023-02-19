85231d0c622c139e55586e1677f32cf004c8e458
MTAKATI <mtksan003@myuct.ac.za>
2023-02-18 16:04:45 -0800

    Displays content to screen using 'puts' function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,11 @@
 /**
  * main - Entry point
  *
- * Return Always 0(Success)
+ * Return: Always 0(Success)
  */
-int main(){
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle \n");
-	return 0;
+	return (0);
 }
