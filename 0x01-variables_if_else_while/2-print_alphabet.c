226e4dfb55d5e1eeb570cff843997a8b0af7b313
MTAKATI <mtksan003@myuct.ac.za>
2023-02-20 04:48:45 -0800

    alphebt

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Answer 0(Success)
+ */
+int main(void)
+{
+	char alphabets[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (int i = 0; i < 26; i++)
+	{
+		putchar(alphabets[i]);
+	}
+	putchar("\n");
+}
