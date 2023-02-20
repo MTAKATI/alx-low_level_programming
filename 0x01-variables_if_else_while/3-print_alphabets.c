9237886a8705e2cf45c9d97036a4ad6d6c77c02d
MTAKATI <mtksan003@myuct.ac.za>
2023-02-20 05:58:07 -0800

    alphets, lower & upper

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <time.h>
 /**
  * main - prints alphabets inlowercase then uppercase
  *
- * Return: Answers 0(Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char letters[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i;
-	
-	for(i = 0; i < 52; i++)
-	{
-		putchar(letters[i]);	
-	}
-	return (0);	
+
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	putchar(i);
+
+	for (i = 'A'; i <= 'Z'; i++)
+	putchar(i);
+	putchar('\n');
+
+	return (0);
+
 }
