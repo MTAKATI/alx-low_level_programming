27070ce3eb283a19a2cee26338882d879540561f
MTAKATI <mtksan003@myuct.ac.za>
2023-02-20 04:57:06 -0800

    compiled right

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,12 @@
 int main(void)
 {
 	char alphabets[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-	for (int i = 0; i < 26; i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alphabets[i]);
 	}
 	putchar('\n');
+	return (0);
 }
