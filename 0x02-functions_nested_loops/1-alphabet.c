2e450dfaff2b972f26cc6bbba15517b25a544944
MTAKATI <mtksan003@myuct.ac.za>
2023-02-21 10:53:49 -0800

    fixed compilation

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,14 +8,6 @@ void print_alphabet(void)
 {
 	int i;
 	for (i = 'a'; i <= 'z'; i++)
-	_putchar(i);
+		_putchar(i);
 	_putchar('\n');
-}
-
-int main(void)
-{
-	
-	print_alphabet();
-	return (0);
-
 }
