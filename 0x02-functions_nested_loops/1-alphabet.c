9c2af25d89f06212f41971dab8854015ad8ef4d2
MTAKATI <mtksan003@myuct.ac.za>
2023-02-21 10:48:30 -0800

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - prints alphabets in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+	_putchar(i);
+	_putchar('\n');
+}
+
+int main(void)
+{
+	
+	print_alphabet();
+	return (0);
+
+}
